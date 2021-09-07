bd25295af618aeeb9d6fb194b8180df179ff197a
Oleksandr Saulko <aleksandrsaulko@gmail.com>
2021-09-07 17:32:53 +0300

    Updates

---
+++ precache-manifest.68c694b0686aa92bd4160fa897ad465c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f1122ea5035158dc715cb16a19133e3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "77a15f88b827e8eb8319",
+    "url": "./static/css/main.ae4f840b.chunk.css"
+  },
+  {
+    "revision": "bef14ca31350064182c6",
+    "url": "./static/js/2.e8d9cca5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.e8d9cca5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77a15f88b827e8eb8319",
+    "url": "./static/js/main.86c92dd0.chunk.js"
+  },
+  {
+    "revision": "e447b2ddaa3314191b11",
+    "url": "./static/js/runtime-main.e90a3edd.js"
+  }
+]);
